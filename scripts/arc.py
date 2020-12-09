6b7ed27a4b987444ceff2ac2ece65f8eb7e39620
WyldeCat <skan1543@gmail.com>
2020-12-09 14:57:28 +0900

    handle policy argument

--- scripts/arc.py
+++ scripts/arc.py
@@ -8,25 +8,9 @@ class ARC(PagePolicy):
 
     # TODO: Implement add_memtrace for ARC
 
-    cache = []
-
     def add_memtrace(self, memref):
-        page_number = self.get_page_number(memref)
-
-        # first reference page
-        if not page_number in self.cache:
-            self.miss_counter += 1
-            # if len(cache) < max_page_entry_count: # there is space
-            if len(self.cache) < self.max_page_entry_count:  # there is space
-                self.cache.append(page_number)  # append ref at end of the list
-            else:  # full
-                print("cache full")
-                self.cache.pop(0)  # evict first element
-                self.cache.append(page_number)  # append at end of the list
-        else:  # page in the list
-            self.hit_counter += 1
-            self.cache.pop(self.cache.index(page_number))  # pop the old one
-            self.cache.append(page_number)  # append new one
+        # TODO 
+        pass
 
     def print_detailed_result(self):
         # TODO
