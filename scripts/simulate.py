88400d3e962006e13d9d2d1d1aa51662cf6c9c29
kang-sooyeon <sooyeon8979@gmail.com>
2020-12-18 22:30:08 +0900

    change num_buffer/2

--- scripts/simulate.py
+++ scripts/simulate.py
@@ -7,7 +7,7 @@ from visualize import write_graph
 
 parser = argparse.ArgumentParser(description = '')
 parser.add_argument('--mem', default = 4096, type = int, help = 'memory size(MB)')
-parser.add_argument('--num_buffer', default = 64, type = int, help = 'num buffer')
+parser.add_argument('--num_buffer', default = 32, type = int, help = 'num buffer')
 parser.add_argument('--num_interval', default = 200, type = int, help = 'num interval')
 parser.add_argument('--size_buffer', default = 16, type = int, help = 'buffer size(M)')
 
@@ -22,6 +22,7 @@ def main():
     output = tag + ".png"
     csv_output = tag + ".csv"
     title = program + "-" + str(args.policy) + "-" + str(args.mem) + "MB"
+
 
     option = str(args.mem) + " " + str(args.policy) + " " + str(args.num_buffer) + " " + str(args.size_buffer) + " " + csv_output +  " " + str(args.num_interval)
     cmd = os.environ["PIN_ROOT"] + "/pin -t ../obj/pps.so " + option + " -- " + args.target
